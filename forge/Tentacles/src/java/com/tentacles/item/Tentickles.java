fd2e9ff4487c5cc5ef1d3b1a5332e0135d334f72
Dylan Goldman <Dylan.Goldman4@gmail.com>
2017-04-08 12:56:50 -0500

    squid spawner

--- forge/Tentacles/src/java/com/tentacles/item/Tentickles.java
+++ forge/Tentacles/src/java/com/tentacles/item/Tentickles.java
@@ -1,14 +1,18 @@
 package com.tentacles.item;
 
+import javax.swing.text.html.parser.Entity;
+
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.IEntityLivingData;
+import net.minecraft.entity.passive.EntitySquid;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class Tentickles extends Item
@@ -18,7 +22,7 @@ public class Tentickles extends Item
 		super();
 		this.setUnlocalizedName("Tentickles");
 		this.setCreativeTab(CreativeTabs.MISC);
-		this.setRegistryName("Tentickles");
+		this.setRegistryName("Tentickles");		
 	}
 	
 	/*
@@ -33,7 +37,14 @@ public class Tentickles extends Item
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, EnumHand hand)
 	{
-		playerIn.addVelocity(0, 1, 0);
+		net.minecraft.entity.Entity ent = EntityList.createEntityByName("Squid", worldIn);
+		if (ent != null)
+		{
+			EntityLiving entityliving = (EntityLiving) ent;
+			ent.setLocationAndAngles(playerIn.posX + 2, playerIn.posY, playerIn.posZ, worldIn.rand.nextFloat() * 360.0F, 0.0F);
+			worldIn.spawnEntityInWorld(ent);
+		}
+		//playerIn.addVelocity(0, 1, 0);
 		return ActionResult.newResult(EnumActionResult.SUCCESS, itemStackIn);
 	}
 	
